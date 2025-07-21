88c7faae0e3745ffa831d2d033ed1a0a0ac1c7eb
SathyaRangaraj-2003 <sathya.2.rangaraj@coforge.com>
2025-07-21 12:04:46 +0530

    day_08 activity

---
+++ Day_08/activities/activity_01.py
@@ -0,0 +1,10 @@
+#activity 01
+'''reassign A with C,B with A,C with B without temporary variables and multiple line of code '''
+
+
+#declaring variables
+a=10
+b=20
+c=30
+a,b,c=c,a,b #reassigning
+print(a,b,c)
---
+++ Day_08/demo.py
@@ -0,0 +1,33 @@
+#simple activity
+
+# t=(
+#     2,3,[3,4,tuple(map(int,input("enter the number: ").split(" ")))]
+# )
+# # t[2][2][0]=10
+# print(t)
+
+
+# v1=5
+# # v2=type(type(v1))
+# # v3=tuple(v2,)
+# v2=(None,print())
+# print(v1 ," ",v2)
+# # print(type(v1))
+# # # tuple(print(),)
+# # print(print())
+
+
+
+# #dynamic allocation:unpacking
+# # a,b,c=((input("1st :"),input("2nd :"),input("3rd :"))
+# # a,b,c=map(int,input("numbers:").split(" "))
+# a,b,c=1,2,input("3rd: ")
+# print(a,b,c)
+
+# #duplicates available ,need to find which index of min value is printed
+# t=(1,3,2,1)
+# print(t.index(min(t)))  
+# print(t.count(min(t))) #count
+
+
+#advanced tuple
