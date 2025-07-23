a9e75bdc350ee49c15f9a91e09ca1a18036c9716
SathyaRangaraj-2003 <sathya.2.rangaraj@coforge.com>
2025-07-23 16:13:54 +0530

    Update activity_02.py

--- Day_10/activities/activity_02.py
+++ Day_10/activities/activity_02.py
@@ -1,5 +1,8 @@
 #one-liner that finds elements common to all sublists
-#intersect 
+#intersect
 
-list=[[1,2,3,4],[2,3,4,5],[3,4,5,6],[4,5,6,7]]
-print(list)
+#list of lists
+l1=[[1,2,3,4],[2,3,4,5],[3,4,5,6],[4,5,6,7]]
+
+# one-liner that finds and prints elements common to all sublists
+print(list(set(l1[0]) & set(l1[1]) & set(l1[2]) & set(l1[3])))
