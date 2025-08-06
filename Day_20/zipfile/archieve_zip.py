c024152ff577933e21a498d5f9be9206a9f97dd6
SathyaRangaraj-2003 <sathya.2.rangaraj@coforge.com>
2025-08-06 13:05:19 +0530

    update

--- Day_20/zipfile/archieve_zip.py
+++ Day_20/zipfile/archieve_zip.py
@@ -1,6 +1,42 @@
+
+#compress
+# import zipfile
+
+# with zipfile.ZipFile('archieve.zip',mode = 'w') as zf:
+#     zf.write('file_01.txt')
+#     zf.write('file_02.txt')
+#     zf.write('file_03.txt')
+
+# #namelist(),extractall()
+# import zipfile
+# with zipfile.ZipFile('archieve.zip',mode = 'r') as zf:
+#     zf.read('file_01.txt')
+#     print(zf.namelist())
+#     zf.extractall()
+
+
+# import zipfile
+
+# with zipfile.ZipFile('archieve.zip',mode = 'r') as zf:
+#     zf.read('file_01.txt')
+
+
+# import zipfile
+
+# with zipfile.ZipFile('archieve.zip','r',compression=zipfile.ZIP_DEFLATED) as zf:
+    
+
+
+
+
+# import zipfile
+
+# with zipfile.ZipFile('archieve.zip','r',compression=zipfile.ZIP_DEFLATED) as zf:
+#     zf.extractall('archieve_zip')
+
 import zipfile
 
-with zipfile.ZipFile('archieve.zip',mode = 'w') as zf:
-    zf.write('file_01.txt')
-    zf.write('file_02.txt')
-    zf.write('file_03.txt')
+with zipfile.ZipFile('archieve.zip','r') as zf:
+    print(zf.namelist() + "\n")
+    file_extract = input("Enter the file to extract")
+    
